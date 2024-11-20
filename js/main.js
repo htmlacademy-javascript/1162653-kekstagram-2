fec6463254dd0355d9005a32b7a29e71bd16eee9
violet-dub <gracealina@yandex.ru>
2024-11-20 22:42:22 +0300

    Удаляет лишний файл

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import { generatePhotos } from './data.js';
 import { initGallery } from './gallery.js';
 import './upload-form.js';
-import './picture-editor.js';
 
 // Создаем данные
 const photos = generatePhotos();
