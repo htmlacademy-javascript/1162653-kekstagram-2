572c7ae5d7a470e8ce1a187464dbc0bdd705c4ae
violet-dub <gracealina@yandex.ru>
2024-12-02 17:12:28 +0300

    Оптимизирует функции

--- js/filter.js
+++
@@ -1,8 +0,0 @@
-const pictiresFilter = document.querySelector('.img-filters');
-
-// Функция для показа фильтра
-const showPictiresFilter = () => {
-  pictiresFilter.classListremove('img-filters--inactive');
-};
-
-export { showPictiresFilter };
--- js/main.js
+++ js/main.js
@@ -2,15 +2,11 @@ import { initGallery } from './gallery.js';
 import { setUploadFormSubmit, closePictureEditor } from './upload-form.js';
 import { getData } from './api.js';
 import { showloadError, initSuccessMessage } from './user-messages.js';
-import { showPictiresFilter } from './filter.js';
 import { showUserPicture } from './user-picture.js';
 
 getData()
   .then((pictures) => {
     initGallery(pictures);
-  })
-  .then(() => {
-    showPictiresFilter();
   })
   .catch(
     (err) => {
